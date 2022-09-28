b47546e8ce71a620aec2b812e4978ee6520b5075
yaoliuchang <liuchangyao92@gmail.com>
2022-09-28 17:49:01 +0800

    init project framework

---
+++ src/main/java/com/mtech/ique/queueservice/model/entity/QueueList.java
@@ -0,0 +1,23 @@
+//package com.mtech.ique.queueservice.model.entity;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import java.util.ArrayList;
+//import java.util.LinkedList;
+//
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Setter
+//@Getter
+//public class QueueList {
+//    private long queueId;
+//    private LinkedList<QueueTicket> queueTickets;
+//    private long estimateWaitingTime;
+//    private String seatTypeName;
+//    private long waitingSize;
+//}
