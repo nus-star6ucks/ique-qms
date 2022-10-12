582baf03e3a29a3ba40e3d04545ca7bf318d6fd5
yaoliuchang <82298559+yaoliuchang@users.noreply.github.com>
2022-10-12 19:17:00 +0800

    Delete QueueRepository.java

--- src/main/java/com/mtech/ique/queueservice/repository/QueueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mtech.ique.queueservice.repository;
-
-import com.mtech.ique.queueservice.model.entity.Queue;
-import org.springframework.data.repository.CrudRepository;
-
-public interface QueueRepository extends CrudRepository<Queue, Long> {
-}
