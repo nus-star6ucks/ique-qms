7b027da7cef24646ed7a580c37b4f43bbb29d767
yaoliuchang <liuchangyao92@gmail.com>
2022-10-12 19:12:08 +0800

    init project framework

--- src/main/java/com/mtech/ique/queueservice/repository/QueueRepository.java
+++ src/main/java/com/mtech/ique/queueservice/repository/QueueRepository.java
@@ -1,7 +1,7 @@
-package com.mtech.ique.queueservice.repository;
-
-import com.mtech.ique.queueservice.model.entity.Queue;
-import org.springframework.data.repository.CrudRepository;
-
-public interface QueueRepository extends CrudRepository<Queue, Long> {
-}
+//package com.mtech.ique.queueservice.repository;
+//
+//import com.mtech.ique.queueservice.model.entity.Queue;
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface QueueRepository extends CrudRepository<Queue, Long> {
+//}
