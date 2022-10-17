2dace2c847b2f8135d724cc7f8ac0219570f2a43
yaoliuchang <liuchangyao92@gmail.com>
2022-10-17 20:45:34 +0800

    commit

--- src/main/java/com/mtech/ique/queueservice/service/QueueManagementService.java
+++ src/main/java/com/mtech/ique/queueservice/service/QueueManagementService.java
@@ -6,13 +6,10 @@ import com.mtech.ique.queueservice.model.entity.User;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
 
 public interface QueueManagementService {
 
     HashMap<String, Object> createTicket(Long queueId);
-
-    List<QueueTicket> getTickets();
 
     HashMap<String, Object> getQueueTicketDetail(Long ticketId);
 
